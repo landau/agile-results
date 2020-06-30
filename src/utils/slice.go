a0cb9dec7a73b74b7b83e04dad797f678667e7aa
Trevor Landau <landau@users.noreply.github.com>
2020-06-30 09:53:48 -0400

    refactor: move IndexOf to utils package

---
+++ src/utils/slice.go
@@ -0,0 +1,11 @@
+package utils
+
+// IndexOf - Returns the index of an element in a slice
+func IndexOf(limit int, predicate func(i int) bool) int {
+	for i := 0; i < limit; i++ {
+		if predicate(i) {
+			return i
+		}
+	}
+	return -1
+}
