c3146b2af53b18c709db8cea454bef093427c6d3
Trevor L <911603+landau@users.noreply.github.com>
2020-07-06 15:51:09 -0400

    feat: Add support for checklist (#8)

--- src/app/app.go
+++ src/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"fmt"
+	"landau/agile-results/src/checklist"
 	"landau/agile-results/src/prompt"
 
 	"github.com/adlio/trello"
@@ -20,17 +21,24 @@ type CardCreator interface {
 	CreateCard(card *trello.Card, extraArgs trello.Arguments) error
 }
 
+// TODO: Refactor the creators and fetchers to be a trello client wrapper in which
+// I wrap the functionality and provide a mockable interface as well. It is
+// quiet burdensome to use the trello api and contously provide interdependent
+// interfaces for all the actions I want to perform.
+
 // Config -
 type Config struct {
-	CardCreator  CardCreator
-	LabelFetcher LabelFetcher
-	ListID       string
-	Logrus       *logrus.Logger
-	Prompter     prompt.Prompter
+	CardCreator      CardCreator
+	ChecklistCreator checklist.Creator
+	HasChecklist     bool
+	LabelFetcher     LabelFetcher
+	ListID           string
+	Logrus           *logrus.Logger
+	Prompter         prompt.Prompter
 }
 
 // RunApp -
-func RunApp(config *Config) error {
+func RunApp(config *Config) (*trello.Card, error) {
 	cardCreator := config.CardCreator
 	logrus := config.Logrus
 	prompter := config.Prompter
@@ -41,15 +49,29 @@ func RunApp(config *Config) error {
 	cardName, err := prompter.Prompt("Card Name: ")
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	labels, err := config.LabelFetcher.Fetch(trello.Defaults())
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	// I think it may be cleaner, def faster, to store things locally.
+	// Through a dot file with json inside? Works nicely as a backup space as well.
+	// Not sure what it would be for yet. I reduce API usage and have data more
+	// readily available. Basically, have everything on hand except for card
+	// creation. In fact, I could use that mechanism as way to build my own
+	// data model. Perhaps a review of the Trello API to determine some capablities
+	// that I am unaware of.
+	//
+	// The domains would then be relevant locally stored Trello data,
+	// the AR domain, which is creating a card in a certain column, making it
+	// easier to add labels, and create linked subtasks from a checklist.
+	// By removing the Trello API, aside from CardCreator
+
+	// Refactor: This method should not be concerned with `prompter`
 	selectedLabels, err := SelectLabelIDs(labels, prompter)
 	logrus.Debugf("Selected %d labels: %v", len(selectedLabels), selectedLabels)
 
@@ -64,7 +86,23 @@ func RunApp(config *Config) error {
 	err = cardCreator.CreateCard(card, trello.Defaults())
 
 	if err != nil {
-		return err
+		return nil, err
+	}
+
+	if config.HasChecklist {
+		items, err := prompter.PromptList("Checklist Items: ")
+
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = config.ChecklistCreator.Create(card, items)
+
+		if err != nil {
+			// If it fails here, do we delete the card? Then we need a CardDeleter :(
+			// See note above about a TrelloWrapper which limits access to the API
+			return nil, err
+		}
 	}
 
 	// TODO: Refactor this to be an expected response like { Card, Curl: {} }?
@@ -73,5 +111,5 @@ func RunApp(config *Config) error {
 		card.Name, card.ID, card.ShortURL, createCurlDelCmd(card.ID),
 	)
 
-	return nil
+	return card, nil
 }
--- src/app/app_test.go
+++ src/app/app_test.go
@@ -2,6 +2,9 @@ package app
 
 import (
 	"fmt"
+	"landau/agile-results/src/checklist"
+	"landau/agile-results/src/prompt"
+	"reflect"
 	"testing"
 
 	"github.com/adlio/trello"
@@ -32,22 +35,6 @@ func Test_createCurlDelCmd(t *testing.T) {
 			}
 		})
 	}
-}
-
-type PrompterReturnValue struct {
-	s   string
-	err error
-}
-
-type MockPrompter struct {
-	calls []string
-	// TODO: How do I nest this and use in the code?????
-	returnValue PrompterReturnValue
-}
-
-func (p *MockPrompter) Prompt(s string) (string, error) {
-	p.calls = append(p.calls, s)
-	return p.returnValue.s, p.returnValue.err
 }
 
 type MockCardCreatorCall struct {
@@ -85,63 +72,56 @@ func TestRunApp(t *testing.T) {
 	logger.SetLevel(logrus.FatalLevel)
 
 	listID := "1234"
-	prompter := &MockPrompter{returnValue: PrompterReturnValue{s: "foo,bar", err: nil}}
-	cardCreator := &MockCardCreator{}
-
 	labels := []*trello.Label{{ID: "abc", Name: "foo"}, {ID: "efg", Name: "bar"}}
-	labelFetcher := &MockLabelFetcher{returnValue: LabelFetcherReturnValue{labels: labels}}
 
-	config := &Config{
-		Logrus:       logger,
-		Prompter:     prompter,
-		ListID:       listID,
-		CardCreator:  cardCreator,
-		LabelFetcher: labelFetcher,
-	}
+	t.Run("Successfully creates a card with labels and checklist", func(t *testing.T) {
+		prompter := &prompt.MockPrompter{
+			PromptReturnValue: prompt.MockPrompterPromptReturnValue{S: "foo,bar", Err: nil},
+		}
+		cardCreator := &MockCardCreator{}
+		labelFetcher := &MockLabelFetcher{returnValue: LabelFetcherReturnValue{labels: labels}}
 
-	t.Run("Successfully creates a card with labels", func(t *testing.T) {
+		checklistCreator := &checklist.MockCreator{
+			CreateReturnValue: checklist.MockCreateReturnValue{},
+		}
+
+		config := &Config{
+			CardCreator:      cardCreator,
+			ChecklistCreator: checklistCreator,
+			LabelFetcher:     labelFetcher,
+			ListID:           listID,
+			Logrus:           logger,
+			Prompter:         prompter,
+		}
+
 		RunApp(config)
 
-		assertCallCount(
-			t,
-			len(labelFetcher.calls),
-			1,
-			"LabelFetcher.Fetch should only be called once",
-		)
+		assertCallCount(t, len(labelFetcher.calls), 1, "LabelFetcher.Fetch call count")
+		assertCallCount(t, len(cardCreator.calls), 1, "CardCreatore.CreateCard call count")
+		assertCallCount(t, len(prompter.PromptCalls), 2, "Prompter.Prompt call count")
+		assertCallCount(t, len(prompter.PromptListCalls), 0, "Prompter.PromptList call count")
 
-		assertCallCount(
-			t,
-			len(cardCreator.calls),
-			1,
-			"CardCreatore.CreateCard should only be called once",
-		)
-
-		assertCallCount(
-			t,
-			len(prompter.calls),
-			2,
-			"Prompter.prompt should be called twice",
-		)
-
-		if prompter.calls[0] != "Card Name: " {
+		if prompter.PromptCalls[0] != "Card Name: " {
 			t.Error("Expected Prompter.Prompt to recevie 'Card Name: '")
 		}
 
 		prompt2 := fmt.Sprintf("Selected 2 labels (%v, %v): ", labels[0].Name, labels[1].Name)
-		if prompter.calls[1] != prompt2 {
-			t.Errorf("Expected Prompter.Prompt to recevie '%s', got '%s", prompt2, prompter.calls[1])
+		if prompter.PromptCalls[1] != prompt2 {
+			t.Errorf("Expected Prompter.Prompt to recevie '%s', got '%s", prompt2, prompter.PromptCalls[1])
 		}
 
-		if prompter.returnValue.s != cardCreator.calls[0].card.Name {
+		if prompter.PromptReturnValue.S != cardCreator.calls[0].card.Name {
 			t.Errorf(
 				"Card name should be = %s, but got %s",
-				prompter.returnValue.s,
+				prompter.PromptReturnValue.S,
 				cardCreator.calls[0].card.Name,
 			)
 		}
 
+		card := cardCreator.calls[0].card
+
 		// TODO: use deepequal on entire list here instead
-		if cardCreator.calls[0].card.IDLabels[0] != labels[0].ID {
+		if card.IDLabels[0] != labels[0].ID {
 			t.Errorf(
 				"Card should have label = %s, but got %s",
 				labels[0].ID,
@@ -149,17 +129,68 @@ func TestRunApp(t *testing.T) {
 			)
 		}
 
-		if cardCreator.calls[0].card.IDLabels[1] != labels[1].ID {
+		if card.IDLabels[1] != labels[1].ID {
 			t.Errorf(
 				"Card should have label = %s, but got %s",
 				labels[1].ID,
 				cardCreator.calls[0].card.IDLabels[1],
 			)
 		}
+
+		assertCallCount(t, len(checklistCreator.Calls), 0, "CheckListCreator.Creator call count")
+	})
+
+	t.Run("Creates a card with a checklist", func(t *testing.T) {
+		prompter := &prompt.MockPrompter{
+			PromptReturnValue:     prompt.MockPrompterPromptReturnValue{S: "foo,bar", Err: nil},
+			PromptListReturnValue: prompt.MockPrompterPromptListReturnValue{Items: []string{"list"}},
+		}
+		cardCreator := &MockCardCreator{}
+		labelFetcher := &MockLabelFetcher{returnValue: LabelFetcherReturnValue{labels: labels}}
+
+		checklistCreator := &checklist.MockCreator{
+			CreateReturnValue: checklist.MockCreateReturnValue{},
+		}
+
+		config := &Config{
+			CardCreator:      cardCreator,
+			ChecklistCreator: checklistCreator,
+			HasChecklist:     true,
+			LabelFetcher:     labelFetcher,
+			ListID:           listID,
+			Logrus:           logger,
+			Prompter:         prompter,
+		}
+
+		RunApp(config)
+
+		card := cardCreator.calls[0].card
+
+		assertCallCount(t, len(prompter.PromptListCalls), 1, "Prompter.PromptList call count")
+		assertCallCount(t, len(checklistCreator.Calls), 1, "CheckListCreator.Creator call count")
+
+		if checklistCreator.Calls[0].Card != card {
+			t.Errorf(
+				"Checklist should have been called with %v, but got %v",
+				card,
+				checklistCreator.Calls[0].Card,
+			)
+		}
+
+		if !reflect.DeepEqual(checklistCreator.Calls[0].Items, []string{"list"}) {
+			t.Errorf(
+				"Checklist should have been called with %v, but got %v",
+				[]string{"list"},
+				checklistCreator.Calls[0].Items,
+			)
+		}
+
 	})
 }
 
 func assertCallCount(t *testing.T, got int, want int, s string) {
+	t.Helper()
+
 	if got != want {
 		t.Errorf("%s = %v, want %v", s, got, want)
 	}
